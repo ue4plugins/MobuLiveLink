0e21767d2e86a4ee06c47b61233ea3fc7449aea0
KirillFromTechAnimTeam <99897702+KirillFromTechAnimTeam@users.noreply.github.com>
2022-08-16 11:14:31 +0200

    Implements build and target for MotionBuilder 2023

---
+++ Source/MobuLiveLinkPlugin2023.Build.cs
@@ -0,0 +1,12 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.IO;
+
+public class MobuLiveLinkPlugin2023 : MobuLiveLinkPluginBase
+{
+	public MobuLiveLinkPlugin2023(ReadOnlyTargetRules Target) : base(Target, "2023")
+	{
+		CppStandard = CppStandardVersion.Cpp17;
+	}
+}
---
+++ Source/MobuLiveLinkPlugin2023.Target.cs
@@ -0,0 +1,11 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+using System.IO;
+
+public class MobuLiveLinkPlugin2023Target : MobuLiveLinkPluginTargetBase
+{
+	public MobuLiveLinkPlugin2023Target(TargetInfo Target) : base(Target, "2023")
+	{}
+}
